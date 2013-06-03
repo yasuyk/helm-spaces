8b4f5a1e3cb823ceee1e341ce45f9b18a1b8057c
Yasuyuki Oka <yasuyk@gmail.com>
2013-06-04 00:35:39 +0900

    Bump version to 0.2

--- helm-spaces.el
+++ helm-spaces.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2013 Yasuyuki Oka
 
 ;; Author: Yasuyuki Oka <yasuyk@gmail.com>
-;; Version: 0.1
+;; Version: 0.2
 ;; URL: https://github.com/yasuyk/helm-spaces
 ;; Package-Requires: ((helm "1.0") (spaces "0.1.0"))
 ;; Keywords: helm frames convenience
